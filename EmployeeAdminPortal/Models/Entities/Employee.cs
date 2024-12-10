ee0501adc48b1ba98c22da696b09a665ae77aa49
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-10 16:35:45 -0700

    Create an Employee entity

---
+++ EmployeeAdminPortal/Models/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeAdminPortal.Models.Entities
+{
+    public class Employee
+    {
+        public Guid Id { get; set; }
+        public required string Name { get; set; } //required =>  to initiaize Employee object we need to specify required properties (in db not null)
+        public required string Email { get; set; }
+        public string? Phone { get; set; } //? => phone can be null => nullable property
+        public decimal Salary { get; set; }
+    }
+}
