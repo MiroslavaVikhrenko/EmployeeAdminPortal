a1d3ae66493d9166b885f8ddc35f20b966e3126c
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-10 18:16:13 -0700

    Create an AddEmployee command and dto

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,4 +1,6 @@
 ﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -24,6 +26,32 @@ namespace EmployeeAdminPortal.Controllers
             var allEmployees = dbContext.Employees.ToList();
 
             return Ok(allEmployees);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            //we use dto to add data (AddEmployeeDto from Models)
+            //map addEmployeeDto and convert into Entity we have => because db context only accept entity and we want to add a new employee to the db
+
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+
+            //Entities are separated from dto => by adding dto we achieve separation of concern
+            //we expose dto to the outside world while entity is an image of the table in db
+            //we could have different names for dbo and entity properties
+
+            dbContext.Employees.Add(employeeEntity); //this line does NOT add a new employee to the db, you need to save the changes yourself
+            dbContext.SaveChanges(); //only here the employee will be saved to the db table
+
+            return Ok(employeeEntity); //ideally we want to return 201 which is CREATED response but to simplify we will just return Ok
+
+            //We did not initialize an id - it will be added for us by entity framework and it will be return to the user as well
         }
     }
 }
---
+++ EmployeeAdminPortal/Models/AddEmployeeDto.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeAdminPortal.Models
+{
+    public class AddEmployeeDto
+    {
+        //the 4 properties from Employee so that we accept these data from user as parameters in Controller
+        public required string Name { get; set; } //required =>  to initiaize Employee object we need to specify required properties (in db not null)
+        public required string Email { get; set; }
+        public string? Phone { get; set; } //? => phone can be null => nullable property
+        public decimal Salary { get; set; }
+    }
+}
