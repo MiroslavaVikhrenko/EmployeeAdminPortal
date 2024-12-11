d9122e834ac4c7ccb5a1c3c13e36be50f9709820
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-10 17:19:52 -0700

    Inject db context and fetch connection string

--- EmployeeAdminPortal/Program.cs
+++ EmployeeAdminPortal/Program.cs
@@ -1,3 +1,6 @@
+using EmployeeAdminPortal.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//injecting db context
+//fetch the connection string from appsettings.json ("DefaultConnection")
+//using EmployeeAdminPortal.Data
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => 
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))); 
 
 var app = builder.Build();
 
