c6f5471d2cb727930dc486e5aafa8a814ff6cb47
MiroslavaVikhrenko <m.vikhrenko@gmail.com>
2024-12-10 17:47:11 -0700

    Add an EmployeesController

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,29 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    //localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        //inject db context 
+        private readonly ApplicationDbContext dbContext;
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        //add endpoints
+
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees = dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+    }
+}
